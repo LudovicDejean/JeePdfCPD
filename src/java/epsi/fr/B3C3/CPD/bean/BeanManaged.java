9ee1e5388e6fab38bad16adc0a0fccc609399d98
LudovicDejean <dejean.ludovic@gmail.com>
2020-02-05 11:58:22 +0100

    reCreation des servlet pour chaques methodes et creation 1 bean

--- src/java/epsi/fr/B3C3/CPD/bean/BeanManaged.java
+++ src/java/epsi/fr/B3C3/CPD/bean/BeanManaged.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package epsi.fr.B3C3.CPD;
+package epsi.fr.B3C3.CPD.bean;
 
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
@@ -12,14 +12,14 @@ import javax.enterprise.context.Dependent;
  *
  * @author dejea
  */
-@Named(value = "updateImageToPdfServlet")
+@Named(value = "beanManaged")
 @Dependent
-public class UpdateImageToPdfServlet {
+public class BeanManaged {
 
     /**
-     * Creates a new instance of UpdateImageToPdfServlet
+     * Creates a new instance of BeanManaged
      */
-    public UpdateImageToPdfServlet() {
+    public BeanManaged() {
     }
     
 }
