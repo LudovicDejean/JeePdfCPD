9675394c00ea8bd330a0b6bab3083452c7ef9140
Bleykx <pi2r34@gmail.com>
2020-02-10 14:38:49 +0100

    camarch

--- src/java/epsi/fr/B3C3/CPD/bean/AleaBean.java
+++ src/java/epsi/fr/B3C3/CPD/bean/AleaBean.java
@@ -4,10 +4,19 @@
  * and open the template in the editor.
  */
 import epsi.fr.B3C3.CPD.classe.ImageToPDF;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 /**
  *
@@ -17,7 +26,9 @@ import javax.annotation.PostConstruct;
 @SessionScoped
 public class AleaBean implements Serializable {
 
-
+    private UploadedFile file;
+    private String cheminImg = "";
+    private String destination = "C:\\temp\\";
     private String fusionImage;
     private String trierImage;
     private String signerImage;
@@ -70,4 +81,42 @@ public class AleaBean implements Serializable {
         ImageToPDF pdf = new ImageToPDF();
         pdf.ReadPdf();
     }
+    
+    public void upload(FileUploadEvent event) {
+
+        file = event.getFile();
+        if (file != null) {
+
+            FacesMessage message = new FacesMessage("Successful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+
+        try {
+            copyFile(event.getFile().getFileName(), event.getFile().getInputstream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void copyFile(String fileName, InputStream in) {
+        try {
+
+            OutputStream out = new FileOutputStream(new File(destination + fileName));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((read = in.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+
+            in.close();
+            out.flush();
+            out.close();
+
+            System.out.println("New file created!");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
